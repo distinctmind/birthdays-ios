98d67925d766a843985c76c665cd3dce9e290717
Jeanlouis <jeanlouisrebello@gmail.com>
2024-03-25 12:58:26 -0400

    Initial Commit

---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by USER on 2024-03-25.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
